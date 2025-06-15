1843dea0b035942418aec6bc04580cbbd388e62b
spacescribe <nandini2417@gmail.com>
2025-06-15 20:58:06 +0530

    Created a template to extract the metadata

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,9 @@
 import json
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from llm_helper import llm
+
 
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
     enriched_posts = []
@@ -13,11 +18,29 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
             print(epost)
 
 def extract_metadata(post):
-    return {
-        'line_count': 10,
-        'language': 'En',
-        'tags': ["Mental health", "Motivation"]
-    }
+    template = '''
+        You are given a LinkedIn post. Extract the the number of lines, language and the tags in this **exact** format and return **only a valid JSON**. No explanation, no code block.
+
+        Requirements:
+        1. Return a valid JSON with **no preamble**, **no explanation**, **no markdown**.
+        2. JSON object should have exactly three keys: line_count, language and tags. 
+        3. tags is an array of text tags. Extract maximum two tags.
+        4. Language should be English or Hinglish (Hinglish means hindi + english)
+
+        Here is the actual post:
+        {post}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"post": post})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException as e:
+        raise OutputParserException("Failed to parse the Json output: " + str(e))
+    return res
 
 if __name__== "__main__":
     process_posts("data/raw_posts.json", "data/processed_posts.json")
