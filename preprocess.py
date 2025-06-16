6fd0ddf9ee34729da8403f3c8ac6468b1c6af6f6
spacescribe <nandini2417@gmail.com>
2025-06-16 23:10:23 +0530

    Added unified tag extraction to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -14,8 +14,47 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
             post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
         
-        for epost in enriched_posts:
-            print(epost)
+    unified_tags=get_unified_tags(enriched_posts)
+
+    for post in enriched_posts:
+        curr_tags=post['tags']
+        new_tags={unified_tags[tag] for tag in curr_tags}
+        post['tags']=list(new_tags)
+    
+    with open(processed_file_path, 'w', encoding='utf-8') as outfile:
+        json.dump(enriched_posts, outfile, indent=4)
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags=set()
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])
+    
+    unique_tags_list = ', '.join(unique_tags)
+
+    template=''' I will give you a list of tags. You need to unify those tags with the below requirements with no explanation and no code block.
+        1. Tags are unified and merged to create a shorter list. 
+            Example 1: "Jobseekers", "Job Hunting" can be all merged into a single tag "Job Search". 
+            Example 2: "Motivation", "Inspiration", "Drive" can be mapped to "Motivation"
+            Example 3: "Personal Growth", "Personal Development", "Self Improvement" can be mapped to "Self Improvement"
+            Example 4: "Scam Alert", "Job Scam" etc. can be mapped to "Scams"
+        2. Each tag should be follow title case convention. example: "Motivation", "Job Search"
+        3. Output should be a JSON object, No preamble
+        3. Output should have mapping of original tag and the unified tag. 
+        For example: {{"Jobseekers": "Job Search",  "Job Hunting": "Job Search", "Motivation": "Motivation}}
+            
+    Here is the list of tags: 
+    {tags}
+    '''
+    pt=PromptTemplate.from_template(template)
+    chain = pt| llm
+    response=chain.invoke(input={"tags": str(unique_tags_list)})
+
+    try:
+        json_parser=JsonOutputParser()
+        res=json_parser.parse(response.content)
+    except OutputParserException as e:
+        raise OutputParserException("Failed to parse the Json output: " + str(e))
+    return res
 
 def extract_metadata(post):
     template = '''
