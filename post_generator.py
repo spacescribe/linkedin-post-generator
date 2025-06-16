80b42cb7738b1e97867476dd5db849e11727d377
spacescribe <nandini2417@gmail.com>
2025-06-16 23:51:17 +0530

    Added context to post generator

--- post_generator.py
+++ post_generator.py
@@ -1,27 +1,45 @@
 from llm_helper import llm
+from few_shot import FewShotPosts
 
-def get_length_str(len):
-    if(len =="Short"):
+fs=FewShotPosts()
+
+def get_lengthgth_str(length):
+    if(length =="Short"):
         return "1 to 5 lines"
-    elif(len=="Medium"):
+    elif(length=="Medium"):
         return "6 to 10 lines"
     return "11 to 15 lines"
 
-def generate_post(len, lang, topic):
-    len_str=get_length_str(len)
+def get_prompt(length, lang, topic):
+    length_str=get_lengthgth_str(length)
 
     prompt = f'''
         Generate a LinkedIn post using the below information. No preamble.
         1. Topic: {topic}
         2. Language: {lang}
-        3. Length: {len_str}
+        3. lengthgth: {length_str}
 
         If Language is Hinglish, it means it is a mix of Hindi and English.
         The script for the generated post should always be in English.
     '''
+
+    examples=fs.get_filtered_posts(length, lang, topic)
+    if len(examples)>0:
+        prompt+="4. Use the writing style as per the below examples: "
+        for i, post in enumerate(examples):
+            post_text=post['text']
+            prompt+=f"\n Example {i+1}:\n{post_text}\n\n"
+            
+            if i==1:
+                break
+    # print(f"The example prompt is: {prompt}")
+    return prompt
+
+def generate_post(length, lang, topic):
+    prompt = get_prompt(length, lang, topic)
     response = llm.invoke(prompt)
     return response.content
 
 if __name__=="__main__":
-    post=generate_post("Short", "English", "Mental Health")
+    post=generate_post("Medium", "English", "Happiness")
     print(post)
